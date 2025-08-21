9fe5f6152098dd1ed2af316859ca950c53f46c46
mahfuz091 <engmahfuz14@gmail.com>
2025-08-21 22:07:53 +0600

    feedback update

--- src/components/TradeIdentity/TradeIdentity.js
+++ src/components/TradeIdentity/TradeIdentity.js
@@ -1,10 +1,23 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, useCallback } from "react";
 import { ReactComponent as ArrowD } from "../../assets/images/icons/arreowD.svg";
 import { ReactComponent as ArrowU } from "../../assets/images/icons/arrowU.svg";
 import { SelectBox, DateBox, TextBox } from "devextreme-react";
 import "./TradeIdentity.scss";
 import Button from "../ui/Button";
 import { ReactComponent as reload } from "../../assets/images/icons/realods.svg";
+import Tabs from "devextreme-react/tabs";
+import {
+  orientations,
+  tabsText,
+  stylingModeLabel,
+  iconPositionLabel,
+  tabsIconAndText,
+  stylingModes,
+  iconPositions,
+  tabsIcon,
+  orientationLabel,
+} from "./tabData";
+const STRICT_WIDTH_CLASS = "strict-width";
 
 const TradeIdentity = () => {
   const [tradeType, setTradeType] = useState("Futures");
@@ -42,13 +55,113 @@ const TradeIdentity = () => {
     }
   }, [isCollapsed]);
 
+  const [orientation, setOrientation] = useState(orientations[0]);
+  const [stylingMode, setStylingMode] = useState(stylingModes[1]);
+  const [iconPosition, setIconPosition] = useState(iconPositions[0]);
+  const [showNavigation, setShowNavigation] = useState(true);
+  const [scrollContent, setScrollContent] = useState(true);
+  const [fullWidth, setFullWidth] = useState(false);
+  const [width, setWidth] = useState("auto");
+  const [rtlEnabled, setRtlEnabled] = useState(false);
+  const [widgetWrapperClasses, setWidgetWrapperClasses] = useState(
+    "widget-wrapper widget-wrapper-horizontal"
+  );
+  const enforceWidthConstraint = useCallback(
+    (shouldRestrictWidth) => {
+      const callback = (prevClasses) => {
+        const restClasses = prevClasses
+          .split(" ")
+          .filter((className) => className !== STRICT_WIDTH_CLASS)
+          .join(" ");
+        const strictWidthClass = shouldRestrictWidth ? STRICT_WIDTH_CLASS : "";
+        return `${restClasses} ${strictWidthClass}`;
+      };
+      setWidgetWrapperClasses(callback);
+    },
+    [setWidgetWrapperClasses]
+  );
+  const stylingModeChanged = useCallback(
+    (e) => {
+      setStylingMode(e.value);
+    },
+    [setStylingMode]
+  );
+  const iconPositionChanged = useCallback(
+    (e) => {
+      setIconPosition(e.value);
+    },
+    [setIconPosition]
+  );
+  const orientationChanged = useCallback(
+    (e) => {
+      const isVertical = e.value === "vertical";
+      const callback = (prevClasses) => {
+        const restClasses = prevClasses
+          .split(" ")
+          .filter(
+            (className) =>
+              className !==
+              (isVertical
+                ? "widget-wrapper-horizontal"
+                : "widget-wrapper-vertical")
+          )
+          .join(" ");
+        return `${restClasses} widget-wrapper-${e.value}`;
+      };
+      setWidgetWrapperClasses(callback);
+      setOrientation(e.value);
+    },
+    [setOrientation, setWidgetWrapperClasses]
+  );
+  const showNavigationChanged = useCallback(
+    (e) => {
+      const shouldRestrictWidth = e.value || scrollContent;
+      enforceWidthConstraint(shouldRestrictWidth);
+      setShowNavigation(e.value);
+    },
+    [scrollContent, setShowNavigation, enforceWidthConstraint]
+  );
+  const scrollContentChanged = useCallback(
+    (e) => {
+      const shouldRestrictWidth = e.value || showNavigation;
+      enforceWidthConstraint(shouldRestrictWidth);
+      setScrollContent(e.value);
+    },
+    [showNavigation, setScrollContent, enforceWidthConstraint]
+  );
+  const fullWidthChanged = useCallback(
+    (e) => {
+      setFullWidth(e.value);
+      setWidth(e.value ? "100%" : "auto");
+    },
+    [setFullWidth, setWidth]
+  );
+  const rtlEnabledChanged = useCallback(
+    (e) => {
+      setRtlEnabled(e.value);
+    },
+    [setRtlEnabled]
+  );
+
   return (
-    <div className="trade-identity-container trade-identity">
-      <div className="tabs">
-        {tabs.map((tab, index) => (
+    <div className='trade-identity-container trade-identity'>
+      <div className='tabs'>
+        <Tabs
+          id='withText'
+          width={width}
+          defaultSelectedIndex={0}
+          rtlEnabled={rtlEnabled}
+          dataSource={tabsText}
+          scrollByContent={scrollContent}
+          showNavButtons={showNavigation}
+          orientation={orientation}
+          stylingMode={stylingMode}
+          iconPosition={iconPosition}
+        />
+        {/* {tabs.map((tab, index) => (
           <button
             key={index}
-            className="tab-button"
+            className='tab-button'
             style={
               index === 0
                 ? { backgroundColor: "#512da8", border: "none" }
@@ -60,12 +173,12 @@ const TradeIdentity = () => {
           >
             {tab}
           </button>
-        ))}
+        ))} */}
       </div>
 
       <div>
         <div
-          className="identity-header"
+          className='identity-header'
           onClick={() => setIsCollapsed(!isCollapsed)}
           style={{
             cursor: "pointer",
@@ -78,163 +191,163 @@ const TradeIdentity = () => {
           <span>Trade Identity: {internalTradeId}</span>
           <span>
             {isCollapsed ? (
-              <ArrowD className="arrow-icon" />
+              <ArrowD className='arrow-icon' />
             ) : (
-              <ArrowU className="arrow-icon" />
+              <ArrowU className='arrow-icon' />
             )}
           </span>
         </div>
 
         <div
           ref={contentRef}
-          className="trade-form-wrapper"
+          className='trade-form-wrapper'
           style={{
             height: height,
             overflow: "hidden",
             transition: "height 0.4s ease",
           }}
         >
-          <div className="trade-form trade_id">
-            <div className="form-row">
-              <div className="form-group sell-select-wrapper">
-                <label htmlFor="tradeType" className="input-label">
+          <div className='trade-form trade_id'>
+            <div className='form-row'>
+              <div className='form-group sell-select-wrapper'>
+                <label htmlFor='tradeType' className='input-label'>
                   Trade Type
                 </label>
                 <SelectBox
-                  id="tradeType"
+                  id='tradeType'
                   dataSource={strategies}
                   value={tradeType}
-                  placeholder="Select"
+                  placeholder='Select'
                   onValueChanged={(e) => setTradeType(e.value)}
-                  className="custom-input"
+                  className='custom-input'
                 />
               </div>
 
-              <div className="form-group sell-select-wrapper">
-                <label htmlFor="internalTradeId" className="input-label">
+              <div className='form-group sell-select-wrapper'>
+                <label htmlFor='internalTradeId' className='input-label'>
                   Internal Trade Id
                 </label>
                 <TextBox
-                  id="internalTradeId"
+                  id='internalTradeId'
                   value={internalTradeId}
-                  className="custom-input"
+                  className='custom-input'
                   readOnly
                 />
               </div>
 
-              <div className="form-group sell-select-wrapper">
-                <label htmlFor="externalTradeId" className="input-label">
+              <div className='form-group sell-select-wrapper'>
+                <label htmlFor='externalTradeId' className='input-label'>
                   External Trade Id:
                 </label>
                 <TextBox
-                  id="externalTradeId"
+                  id='externalTradeId'
                   value={""}
-                  className="custom-input"
+                  className='custom-input'
                   onValueChanged={() => {}}
                 />
               </div>
 
-              <div className="form-group sell-select-wrapper">
-                <label htmlFor="tradeDate" className="input-label">
+              <div className='form-group sell-select-wrapper'>
+                <label htmlFor='tradeDate' className='input-label'>
                   Trade Date:
                 </label>
                 <DateBox
-                  id="tradeDate"
+                  id='tradeDate'
                   value={tradeDate}
                   onValueChanged={(e) => setTradeDate(e.value)}
-                  displayFormat="dd-MM-yyyy"
-                  className="custom-input"
+                  displayFormat='dd-MM-yyyy'
+                  className='custom-input'
                 />
               </div>
 
-              <div className="form-group sell-select-wrapper">
-                <label htmlFor="trader" className="input-label">
+              <div className='form-group sell-select-wrapper'>
+                <label htmlFor='trader' className='input-label'>
                   Trader
                 </label>
                 <SelectBox
-                  id="trader"
+                  id='trader'
                   dataSource={traders}
                   value={trader}
-                  placeholder="Select"
+                  placeholder='Select'
                   onValueChanged={(e) => setTrader(e.value)}
-                  className="custom-input"
+                  className='custom-input'
                 />
               </div>
 
-              <div className="form-group sell-select-wrapper">
-                <label htmlFor="operator" className="input-label">
+              <div className='form-group sell-select-wrapper'>
+                <label htmlFor='operator' className='input-label'>
                   Operator
                 </label>
                 <SelectBox
-                  id="operator"
+                  id='operator'
                   dataSource={operators}
                   value={operator}
-                  placeholder="Select"
+                  placeholder='Select'
                   onValueChanged={(e) => setOperator(e.value)}
-                  className="custom-input"
+                  className='custom-input'
                 />
               </div>
 
-              <div className="form-group sell-select-wrapper">
-                <label htmlFor="status" className="input-label">
+              <div className='form-group sell-select-wrapper'>
+                <label htmlFor='status' className='input-label'>
                   Status
                 </label>
                 <SelectBox
-                  id="status"
+                  id='status'
                   dataSource={statuses}
                   value={status}
-                  placeholder="Select"
+                  placeholder='Select'
                   onValueChanged={(e) => setStatus(e.value)}
-                  className="custom-input"
+                  className='custom-input'
                 />
               </div>
             </div>
 
-            <div className="form-row2">
-              <div className="">
-                <div className="Strategy">
-                  <label htmlFor="strategy" className="input-label">
+            <div className='form-row2'>
+              <div className=''>
+                <div className='Strategy'>
+                  <label htmlFor='strategy' className='input-label'>
                     Strategy
                   </label>
-                  <label className="input-label">Book: Demo Products</label>
+                  <label className='input-label'>Book: Demo Products</label>
                 </div>
                 <SelectBox
-                  id="strategy"
+                  id='strategy'
                   dataSource={strategies}
                   value={strategy}
-                  placeholder="Select"
+                  placeholder='Select'
                   onValueChanged={(e) => setStrategy(e.value)}
-                  className="custom-input"
+                  className='custom-input'
                 />
               </div>
 
               <Button icon={reload} />
 
-              <div className="">
-                <label htmlFor="tickets" className="input-label">
+              <div className=''>
+                <label htmlFor='tickets' className='input-label'>
                   Tickets | Description
                 </label>
                 <SelectBox
-                  id="tickets"
+                  id='tickets'
                   dataSource={ticketsList}
                   value={tickets}
-                  placeholder="Select"
+                  placeholder='Select'
                   onValueChanged={(e) => setTickets(e.value)}
-                  className="custom-input"
+                  className='custom-input'
                 />
               </div>
 
-              <div className="">
-                <label htmlFor="productCode" className="input-label">
+              <div className=''>
+                <label htmlFor='productCode' className='input-label'>
                   Product Code
                 </label>
                 <SelectBox
-                  id="productCode"
+                  id='productCode'
                   dataSource={statuses}
                   value={productCode}
-                  placeholder="Select"
+                  placeholder='Select'
                   onValueChanged={() => {}}
-                  className="custom-input"
+                  className='custom-input'
                   readOnly
                 />
               </div>
